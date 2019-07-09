a37662891e86079b039bc84404158e9d2de16fca
larsfinlay <larsfinlay@googlemail.com>
2019-07-09 13:05:49 +0100

    add.Should throw an exception when ANY arguments passed are not integers :: Green

--- Ruby/lib/number_factory.rb
+++ Ruby/lib/number_factory.rb
@@ -3,6 +3,14 @@
 # A class for completing simple mathematical functions
 class NumberFactory
   def add(int_a, int_b)
+    validate_int(int_a)
+    validate_int(int_b)
     int_a + int_b
+  end
+
+  private
+
+  def validate_int(int)
+    raise("Non int argument; '#{int}' passed") unless int.instance_of? Integer
   end
 end
--- Ruby/spec/spec_helper.rb
+++ Ruby/spec/spec_helper.rb
@@ -1,4 +1,9 @@
 require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [SimpleCov::Formatter::Console]
+)
 SimpleCov.start
 
 RSpec.configure do |config|
@@ -9,6 +14,6 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-  
+
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
