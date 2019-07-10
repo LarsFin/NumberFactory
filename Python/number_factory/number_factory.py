b85f22b24c0c74e28c992d0773da23bdc88dac63
larsfinlay <larsfinlay@googlemail.com>
2019-07-10 12:06:43 +0100

    test_add_arguments :: Green

--- Python/number_factory/number_factory.py
+++ Python/number_factory/number_factory.py
@@ -3,7 +3,17 @@ import sys
 
 class NumberFactory:
     def add(self, int_a, int_b):
+        self._validate_int(int_a)
+        self._validate_int(int_b)
         return int_a + int_b
 
     def subtract(self, int_a, int_b):
         return int_a - int_b
+
+    def _validate_int(self, arg):
+        if not isinstance(arg, int):
+            raise ArgumentException("Argument: {} is not an int".format(int))
+
+
+class ArgumentException(Exception):
+    pass
