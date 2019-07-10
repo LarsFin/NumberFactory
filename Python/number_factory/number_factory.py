22ea851f04a2107ac87a2eb9029646f35900d935
larsfinlay <larsfinlay@googlemail.com>
2019-07-10 12:22:31 +0100

    test_divide :: Green

--- Python/number_factory/number_factory.py
+++ Python/number_factory/number_factory.py
@@ -17,6 +17,9 @@ class NumberFactory:
         self._validate_int(int_b)
         return int_a * int_b
 
+    def divide(self, int_a, int_b):
+        return float(int_a) / int_b
+
     def _validate_int(self, arg):
         if not isinstance(arg, int):
             raise ArgumentException("Argument: {} is not an int".format(int))
