42a137c5336a833674465d64adb24d0a7d8d0e2f
larsfinlay <larsfinlay@googlemail.com>
2019-07-10 11:48:17 +0100

    test_subtract :: Green

--- Python/number_factory/number_factory.py
+++ Python/number_factory/number_factory.py
@@ -7,3 +7,6 @@ class NumberFactory:
 
     def add(self, int_a, int_b):
         return int_a + int_b
+
+    def subtract(self, int_a, int_b):
+        return int_a - int_b
--- Python/tests/test_number_factory.py
+++ Python/tests/test_number_factory.py
@@ -18,14 +18,14 @@ class NumberFactoryTest(unittest.TestCase):
         actual_result = self.number_factory.add(INT_A, INT_B)
         fail_message = "Expected {expected}, got {actual}.".format(
             expected=expected_result, actual=actual_result)
-        self.assertEqual(actual_result, expected_result)
+        self.assertEqual(actual_result, expected_result, fail_message)
 
     def test_subtract(self):
         expected_result = 4
         actual_result = self.number_factory.subtract(INT_A, INT_B)
         fail_message = "Expected {expected}, got {actual}.".format(
             expected=expected_result, actual=actual_result)
-        self.assertEqual(actual_result, expected_result)
+        self.assertEqual(actual_result, expected_result, fail_message)
 
 
 if __name__ == "__main__":
