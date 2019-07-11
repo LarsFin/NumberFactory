4a598bd85782c3cb111e0a99cd84af767a38db6b
larsfinlay <larsfinlay@googlemail.com>
2019-07-11 12:39:24 +0100

    testAddReturn() :: Red

---
+++ PHP/tests/NumberFactoryTest.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+include './src/NumberFactory.php';
+
+use PHPUnit\Framework\TestCase;
+
+final class NumberFactoryTest extends TestCase
+{
+    private $INT_A = 3;
+    private $INT_B = 10;
+
+    public function testAddReturn(): void
+    {
+        $expected = 13;
+        $numberFactory = new NumberFactory;
+        $this->assertSame(
+            $expected,
+            $numberFactory.add($INT_A, $INT_B)
+        );
+    }
+}
+?>
