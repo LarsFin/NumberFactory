25ca6b18238f2c70606f868035903a1a8204cd11
larsfinlay <larsfinlay@googlemail.com>
2020-04-22 14:22:43 +0100

    NumberFactory::add - Red

--- TypeScript/test/number-factory.test.ts
+++ TypeScript/test/number-factory.test.ts
@@ -1,3 +1,25 @@
-test("Sample Test", () => {
-  expect(1 + 1).toBe(2);
+import { NumberFactory } from "../src/number-factory";
+
+let numberFactory: NumberFactory;
+
+beforeEach(() => {
+
+  numberFactory = new NumberFactory();
+
+});
+
+describe("addition tests", () => {
+
+  test("returns correct value", () => {
+
+    const n1 = 5;
+    const n2 = 10;
+    const expected = 15;
+
+    const result: number = numberFactory.add(n1, n2);
+
+    expect(result).toBe(expected);
+
+  });
+
 });
